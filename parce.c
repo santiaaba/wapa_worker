9aa46502a6a294371e4da90a67ab56836ea1d585
Santiago Miguel Diaz <santiaaba@hotmail.com>
2018-07-31 16:41:49 -0400

    cambios en agregar un sitio

---
+++ parce.c
@@ -0,0 +1,38 @@
+#include "parce.h"
+
+void parce_data(char *buffer, int *i, char *value){
+	/* Dado un comando recibido desde el servidor con formato
+	   de campos separados por |, retorna el valor a partir
+	   de la posición dada hasta el proximo | o fin del string.
+	   Retorna ademas la posición fin en i. los datos comienzan
+	   en la posicion 3*/
+
+	int j = 0;
+	int largo;
+
+	largo = strlen(buffer);
+	printf("Parceando %s\n",buffer);
+	while(*i < largo && buffer[*i] != '|' && buffer[*i] != '\0'){
+		printf("la i es: %i\n",*i);
+		printf("%c\n",buffer[*i]);
+		value[j] = buffer[*i];
+		j++; (*i)++;
+	}
+	value[j]='\0';
+	/* Adelantamos la i una posicion ya que se
+ 	   encuentra parada en el caracter "|" posiblemente */
+	(*i)++;
+	printf("valor: %s\n",value);
+}
+char parce_get_action(char *buffer){
+	/* Dado el buffer retorna la accion a realizar. El mismo
+	   es el char en la posicion 2 */
+	return buffer[2];
+}
+void parce_get_taskid(char *buffer, char *value){
+	/* Dado un buffer, retorna el taskid como
+	   una secuencia de 2 bytes (dos char). Se
+	   encuentran en la posición 0 a la 1. */
+	value[0] = buffer[0];
+	value[1] = buffer[1];
+}
---
+++ parce.h
@@ -0,0 +1,5 @@
+#include <string.h>
+
+void parce_data(char *buffer, int *i, char *value);
+char parce_get_action(char *buffer);
+void parce_get_taskid(char *buffer, char *value);
